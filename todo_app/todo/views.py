1dd2603f7dd7f296c4ba694e8010e1fd593a3603
ridzRUSH <12adarshpandey12@gmail.com>
2024-12-10 20:21:01 +0530

    added view to todo

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('list_tasks')
+    return render(request, 'tasks/add_task.html')
+
+def list_tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list_tasks.html', {'tasks': tasks})
